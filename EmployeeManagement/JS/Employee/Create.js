645023b4828c9e0c10b0ce3e9c64096276697e1b
Jesse Zhang <uncle.chao@live.cn>
2015-05-06 14:20:16 +0800

    Delete button approach

--- EmployeeManagement/JS/Employee/Create.js
+++ EmployeeManagement/JS/Employee/Create.js
@@ -1,6 +1,7 @@
 ﻿//==========Page Load==========
 $(function () {
     var clickCount = -1;
+    $("#deleteEducationInfo").attr("disabled", true);  
     $('#createEducationInfo').click(function () {
         clickCount++;
         $.ajax({
@@ -10,9 +11,15 @@ $(function () {
             url: "/Employee/AddEducationInfo",
             success: function (data) {
                 $("#EducationInfoShow").append(data);
+                $("#deleteEducationInfo").attr("disabled", false);
             }
         });
     });
-    $('#deleteEducationInfo').click(function () {
+    $("#deleteEducationInfo").click(function () {
+        $("#EducationInfoShow ul:last-child").remove();
+        clickCount--;
+        if ($("#EducationInfoShow").html().length <500) {
+            $("#deleteEducationInfo").attr("disabled", true);
+        }
     });
 });
--- EmployeeManagement/JS/Employee/Edit.js
+++ EmployeeManagement/JS/Employee/Edit.js
@@ -2,8 +2,9 @@
 $(function () {
     //Edit时，需要判断新增的EducationInfo为第几个，很可能不是从0开始
     var clickCount = $("#existEducationCount").val();
+    $("#deleteEducationInfo").attr("disabled", true);
+
     $('#createEducationInfo').click(function () {
-        alert(clickCount);
         $.ajax({
             type: "POST",
             data: { index: clickCount },
@@ -11,8 +12,17 @@ $(function () {
             url: "/Employee/AddEducationInfo",
             success: function (data) {
                 $("#EducationInfoShow").append(data);
+                $("#deleteEducationInfo").attr("disabled", false);
             }
         });
         clickCount++;
+    });
+    $("#deleteEducationInfo").click(function () {
+        $("#EducationInfoShow ul:last-child").remove();
+        clickCount--;
+        alert($("#EducationInfoShow").html().length);
+        if ($("#EducationInfoShow").html().length < 500) {
+            $("#deleteEducationInfo").attr("disabled", true);
+        }
     });
 });
