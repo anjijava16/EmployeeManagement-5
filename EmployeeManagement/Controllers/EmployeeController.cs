a5ead39627c35f186630479a014c483aef9bda97
Jesse Zhang <uncle.chao@live.cn>
2015-05-06 23:31:03 +0800

    Add simultaneously update control

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -65,44 +65,61 @@ namespace EmployeeManagement.Controllers
         {
             if (ModelState.IsValid)
             {
-                var employee4update = db.EmployeeSet.Where(e => e.Id == employee.Id).SingleOrDefault();
-                employee4update.Name = employee.Name;
-                employee4update.Age = employee.Age;
-                employee4update.Seniority = employee.Seniority;
-                if (employee.EducationInfo != null)
+                List<byte> temp = employee.RowVersion.ToList();
+                List<byte> newest = db.EmployeeSet.Where(e => e.Id == employee.Id).SingleOrDefault().RowVersion.ToList();
+                if (ListCompare(temp, newest))
                 {
-                    var educationInfo = employee.EducationInfo.ToList();
-                    int count = educationInfo.Count;
-                    for (int i = 0; i < count; i++)
+                    var employee4update = db.EmployeeSet.Where(e => e.Id == employee.Id).SingleOrDefault();
+                    employee4update.Name = employee.Name;
+                    employee4update.Age = employee.Age;
+                    employee4update.Seniority = employee.Seniority;
+                    if (employee.EducationInfo != null)
                     {
-                        //judge if the education info exist or not
-                        int uid = educationInfo[i].Id;
-                        var ret = db.EducationInfoSet.Where(e => e.Id == uid).SingleOrDefault();
-                        if (ret != null)
+                        var educationInfo = employee.EducationInfo.ToList();
+                        int count = educationInfo.Count;
+                        for (int i = 0; i < count; i++)
                         {
-                            //exist
-                            ret.StartTime = educationInfo[i].StartTime;
-                            ret.EndTime = educationInfo[i].EndTime;
-                            ret.Education = educationInfo[i].Education;
-                            ret.IsDel = educationInfo[i].IsDel;
-                        }
-                        else
-                        {
-                            //need to add
-                            EducationInfo eduInfo = new EducationInfo();
-                            eduInfo.EmployeeId = employee.Id;
-                            eduInfo.IsDel = false;
-                            eduInfo.StartTime = educationInfo[i].StartTime;
-                            eduInfo.EndTime = educationInfo[i].EndTime;
-                            eduInfo.Education = educationInfo[i].Education;
-                            db.EducationInfoSet.Add(eduInfo);
+                            //judge if the education info exist or not
+                            int uid = educationInfo[i].Id;
+                            var ret = db.EducationInfoSet.Where(e => e.Id == uid).SingleOrDefault();
+                            if (ret != null)
+                            {
+                                //exist
+                                List<byte> eduInfo = educationInfo[i].RowVersion.ToList();
+                                List<byte> newestEduInfo = db.EducationInfoSet.Where(e => e.Id == uid).SingleOrDefault().RowVersion.ToList();
+                                if (ListCompare(eduInfo, newestEduInfo))
+                                {
+                                    ret.StartTime = educationInfo[i].StartTime;
+                                    ret.EndTime = educationInfo[i].EndTime;
+                                    ret.Education = educationInfo[i].Education;
+                                    ret.IsDel = educationInfo[i].IsDel;
+                                }
+                                else
+                                {
+                                    return Content("<script>alert('UPDATE ERROR');location.href='/Employee/Index'</script>");
+                                }
+                            }
+                            else
+                            {
+                                //need to add
+                                EducationInfo eduInfo = new EducationInfo();
+                                eduInfo.EmployeeId = employee.Id;
+                                eduInfo.IsDel = false;
+                                eduInfo.StartTime = educationInfo[i].StartTime;
+                                eduInfo.EndTime = educationInfo[i].EndTime;
+                                eduInfo.Education = educationInfo[i].Education;
+                                db.EducationInfoSet.Add(eduInfo);
+                            }
                         }
                     }
+                    //db.Entry(employee).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
                 }
-                //ModelState.AddModelError(string.Empty, "提示");
-                //db.Entry(employee).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                else
+                {
+                    return Content("<script>alert('UPDATE ERROR');location.href='/Employee/Index'</script>");
+                }
             }
             return View(employee);
         }
@@ -146,6 +163,17 @@ namespace EmployeeManagement.Controllers
         {
             db.Dispose();
             base.Dispose(disposing);
+        }
+
+
+        private bool ListCompare(List<byte> l1, List<byte> l2)
+        {
+            bool flag = true;
+            for (int i = 0; i < l1.Count; i++)
+            {
+                if (l1[i] != l2[i]) flag = false;
+            }
+            return flag;
         }
     }
 }
