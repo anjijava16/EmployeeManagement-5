51cc824dfdcc4b7851bd2cd7bc0511d9e71c6f2c
Jesse Zhang <uncle.chao@live.cn>
2015-05-07 15:22:58 +0800

    Fix bug
    MVC client validation after PartialView loaded via Ajax

--- EmployeeManagement/JS/Employee/Create.js
+++ EmployeeManagement/JS/Employee/Create.js
@@ -1,24 +1,31 @@
 ﻿//==========Page Load==========
 $(function () {
     var clickCount = -1;
-    $("#deleteEducationInfo").attr("disabled", true);  
+    $("#deleteEducationInfo").attr("disabled", true);
     $('#createEducationInfo').click(function () {
         clickCount++;
         $.ajax({
             type: "POST",
             data: { index: clickCount },
-            //dataType: "json",
             url: "/Employee/AddEducationInfo",
             success: function (data) {
                 $("#EducationInfoShow").append(data);
                 $("#deleteEducationInfo").attr("disabled", false);
+
+                $("form").removeData("validator");
+                $("form").removeData("unobtrusiveValidation");
+                $.validator.unobtrusive.parse("form");
             }
         });
+
     });
     $("#deleteEducationInfo").click(function () {
         $("#EducationInfoShow ul:last-child").remove();
+        $("form").removeData("validator");
+        $("form").removeData("unobtrusiveValidation");
+        $.validator.unobtrusive.parse("form");
         clickCount--;
-        if ($("#EducationInfoShow").html().length <500) {
+        if ($("#EducationInfoShow").html().length < 500) {
             $("#deleteEducationInfo").attr("disabled", true);
         }
     });
--- EmployeeManagement/JS/Employee/Edit.js
+++ EmployeeManagement/JS/Employee/Edit.js
@@ -12,12 +12,18 @@ $(function () {
             success: function (data) {
                 $("#EducationInfoShow").append(data);
                 $("#deleteEducationInfo").attr("disabled", false);
+                $("form").removeData("validator");
+                $("form").removeData("unobtrusiveValidation");
+                $.validator.unobtrusive.parse("form");
             }
         });
         clickCount++;
     });
     $("#deleteEducationInfo").click(function () {
         $("#EducationInfoShow ul:last-child").remove();
+        $("form").removeData("validator");
+        $("form").removeData("unobtrusiveValidation");
+        $.validator.unobtrusive.parse("form");
         clickCount--;
         if ($("#EducationInfoShow").html().length < 500) {
             $("#deleteEducationInfo").attr("disabled", true);
